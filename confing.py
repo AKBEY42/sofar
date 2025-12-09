7912436977f6744e32f0160d9dadd5ea7ea38640
AKBEY42 <inan_akbey@hotmail.com>
2025-12-09 19:24:39 +0300

    Update confing.py

--- confing.py
+++ confing.py
@@ -1,5 +1,5 @@
+TOKEN = 8515512057:AAGODA4CJ3MEAWO8QgG3PYiDB6OlWUpnNV4
 import os
-
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "sofar-secret-key")
     DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///sofar.db")
